70cd49a4c43f173fe554b64ea88f3b934e0b217e
Gedan Magalhães <gedanycar@gmail.com>
2019-08-09 11:24:11 -0300

    Classe 'Altera Empresa' removida

--- src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -12,6 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns="/oi")
 public class OiMundoServlet extends HttpServlet {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
